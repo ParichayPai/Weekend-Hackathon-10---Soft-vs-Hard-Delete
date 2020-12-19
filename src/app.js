75433673e08ffe3ecc4bcdd8ea90310dd96960d4
ParichayPai <parichaypai393@gmail.com>
2020-12-19 18:53:27 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -11,22 +11,71 @@ app.use(express.json());
 
 // Get all the students
 app.get('/students', async (req, res) => {
-    // write your codes here
+    try{
+        const students = await Student.find();
+        res.status(200).json(students);
+    } catch(err) {
+        res.status(404).json({
+            err
+        })
+    }
 })
 
 // Add student to database
 app.post('/students', async (req, res) =>{
     // write your codes here
+    try{
+        const student = await Student.create(req.body);
+        res.status(201).json(student);
+    }catch(err){
+        res.status(400).json({
+            err
+        })
+    }
 })
 
 // Get specific student
 app.get('/students/:id', async (req, res) =>{
     // write your codes here
+    try{
+        const student = await Student.findById(req.params.id);
+        res.status(200).json(student)
+    }catch(err){
+        res.status(404).json({
+            err
+        })
+    }
 })
 
 // delete specific student
 app.delete('/students/:id', async (req, res) =>{
     // write your codes here
+    try{
+        const query = req.query.type;
+        if(query === 'hard'){
+            await Student.findByIdAndDelete(req.params.id);
+            res.status(204).json({
+                status:"success",
+                data: null
+            });
+        }else if(query === 'soft'){
+            const student = await Student.findByIdAndUpdate(req.params.id, {isDeleted: true}, {
+                new: true,
+                runValidators: true
+            });
+            res.status(200).json({
+                status:"success",
+                data:{
+                    student
+                }
+            });
+        }
+    }catch(err){
+        res.status(500).json({
+            status:"fail",
+            message: err
+        });
+    }
 }) 
 
 
